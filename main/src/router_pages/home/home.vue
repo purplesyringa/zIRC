503c35bb04ac481c8ff74a6c95b262dd2434d4ce
Ivanq <imachug@yandex.ru>
2018-10-02 07:17:57 +0300

    Send messages to console

--- main/src/router_pages/home/home.vue
+++ main/src/router_pages/home/home.vue
@@ -1,11 +1,46 @@
 <template>
-	<div>
-		Home
-	</div>
+	<main>
+		<input
+			type="text"
+			v-model="message"
+			ref="message"
+			class="message"
+			@keypress.enter="submit"
+		/>
+		<button @click="submit">Send &gt;</button>
+	</main>
 </template>
+
+<style lang="sass" scoped>
+	main
+		padding: 32px
+
+		.message, button
+			font-family: "Courier New", monospace
+			font-size: 16px
+			padding: 8px 12px
+</style>
 
 <script type="text/javascript">
 	export default {
-		name: "Home"
+		name: "Home",
+		data() {
+			return {
+				message: ""
+			};
+		},
+
+		methods: {
+			submit() {
+				if(!this.message.trim()) {
+					return;
+				}
+
+				console.log(this.message.trim());
+
+				this.message = "";
+				this.$refs.message.focus();
+			}
+		}
 	};
 </script>
