7933721ebb9bef5ba8baf5ae16c30ebedbf34489
Ivanq <imachug@yandex.ru>
2018-10-05 22:42:04 +0300

    Sort messages by receive date, and if it's not set, by send date

--- main/src/libs/irc/speakable.js
+++ main/src/libs/irc/speakable.js
@@ -21,14 +21,16 @@ export default class Speakable extends EventEmitter {
 
 	async loadHistory() {
 		if(!this.history) {
-			// Get history by reading data files
-			this.history = await this._loadHistory();
-			for(const message of this.history) {
+			this.history = [];
+
+			// First load history from permanent storage
+			for(const message of await Storage.load(this.name)) {
 				this.received[message.message.id] = true;
+				this.history.push(message);
 			}
 
-			// And now load history from permanent storage
-			for(const message of await Storage.load(this.name)) {
+			// And now history by reading data files
+			for(const message of await this._loadHistory()) {
 				if(this.received[message.message.id]) {
 					continue;
 				}
@@ -36,6 +38,12 @@ export default class Speakable extends EventEmitter {
 				this.received[message.message.id] = true;
 				this.history.push(message);
 			}
+
+			this.history.sort((a, b) => {
+				const aDate = a.receiveDate || a.message.date;
+				const bDate = b.receiveDate || b.message.date;
+				return aDate - bDate;
+			});
 		}
 
 		return this.history;
