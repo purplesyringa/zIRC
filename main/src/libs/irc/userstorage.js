1e0ab36d095d37aa866c453ce02486a68c0434cb
Ivanq <imachug@yandex.ru>
2018-12-14 19:05:33 +0300

    Separate data for each user

--- main/src/libs/irc/userstorage.js
+++ main/src/libs/irc/userstorage.js
@@ -1,10 +1,16 @@
 import {zeroPage} from "zero";
 
 export default new class UserStorage {
+	async getAuthAddress() {
+		return (await zeroPage.getSiteInfo()).auth_address;
+	}
+
 	async get() {
-		return await zeroPage.cmd("userGetSettings");
+		return ((await zeroPage.cmd("userGetSettings")) || {})[await this.getAuthAddress()];
 	}
 	async set(settings) {
-		await zeroPage.cmd("userSetSettings", [settings]);
+		const globalSettings = (await zeroPage.cmd("userGetSettings")) || {};
+		globalSettings[await this.getAuthAddress()] = settings;
+		await zeroPage.cmd("userSetSettings", [globalSettings]);
 	}
 };
