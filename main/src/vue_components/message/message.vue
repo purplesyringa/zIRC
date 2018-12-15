24ad641d397807b75817598853d57847434141d0
Ivanq <imachug@yandex.ru>
2018-12-15 14:17:43 +0300

    Use flex for messages

--- main/src/vue_components/message/message.vue
+++ main/src/vue_components/message/message.vue
@@ -2,17 +2,18 @@
 	<div class="message">
 		<Avatar class="avatar" :channel="certUserId || `@${authAddress}`" />
 
-		<div class="header">
-			<span class="user" v-if="certUserId">{{certUserId}}</span>
-			<span class="user anonymous" v-else>Anonymous</span>
-			<span class="date" :title="title">[{{dateText}}]</span>
-		</div>
-		<div class="messages">
-			<div v-for="message in messages" class="message-item">
-				{{message.text}}
+		<div class="content">
+			<div class="header">
+				<span class="user" v-if="certUserId">{{certUserId}}</span>
+				<span class="user anonymous" v-else>Anonymous</span>
+				<span class="date" :title="title">[{{dateText}}]</span>
+			</div>
+			<div class="messages">
+				<div v-for="message in messages" class="message-item">
+					{{message.text}}
+				</div>
 			</div>
 		</div>
-		<ClearFix />
 	</div>
 </template>
 
@@ -21,26 +22,31 @@
 		margin: 16px 0
 		font-family: "Courier New", monospace
 
+		display: flex
+		flex-direction: row
+
 		.avatar
 			float: left
 			margin-right: 16px
+			flex: 0 0 64px
 
-		.header
-			padding: 8px 0
-		.user
-			font-weight: bold
-		.anonymous
-			font-style: italic
-			color: #FCC
-		.date
-			font-style: italic
-			color: #888
+		.content
+			.header
+				padding: 8px 0
+			.user
+				font-weight: bold
+			.anonymous
+				font-style: italic
+				color: #FCC
+			.date
+				font-style: italic
+				color: #888
 
-		.messages
-			float: left
-			margin: -8px 0
-			.message-item
-				margin: 8px 0
+			.messages
+				float: left
+				margin: -8px 0
+				.message-item
+					margin: 8px 0
 </style>
 
 <script type="text/javascript">
