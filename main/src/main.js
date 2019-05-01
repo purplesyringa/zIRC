321cca86fb4d8c5dce11324f49a08caf68703b68
Ivanq <imachug@yandex.ru>
2019-05-01 23:14:43 +0300

    Send pings every 40 seconds

--- main/src/main.js
+++ main/src/main.js
@@ -93,3 +93,17 @@ zeroPage.on("setSiteInfo", msg => {
 	store.commit("setSiteInfo", msg.params);
 	app.$eventBus.$emit("setSiteInfo", msg.params);
 });
+
+
+// Send pings
+import PeerTransport from "libs/irc/transport/peer";
+setInterval(() => {
+	PeerTransport.send(null, {
+		cmd: "ping"
+	});
+}, 40000);
+setTimeout(() => {
+	PeerTransport.send(null, {
+		cmd: "ping"
+	});
+}, 5000);
